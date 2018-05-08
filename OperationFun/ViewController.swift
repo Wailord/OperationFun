5500e3c4c606cd810fa96cc1092dd6f8167dfc9f
Wailord <ryan@wailord.com>
2018-05-08 12:47:21 -0500

    Initial Commit

---
+++ OperationFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OperationFun
+//
+//  Created by Ryan Fox on 5/8/18.
+//  Copyright © 2018 Wailord. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
